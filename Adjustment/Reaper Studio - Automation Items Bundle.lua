162e448b5bcb1a7064cfb15c882b3327ff6c35a0
BIXI DOX <bixidox4@gmail.com>
2025-11-29 21:42:55 +0700

    Metapackaging scripts test
    Yeah, I did that.

---
+++ Adjustment/Reaper Studio - Automation Items Bundle.lua
@@ -0,0 +1,16 @@
+--  @description: Reaper Studio - Automation Items Bundle
+--  @version: 1.0
+--  @author: BIXIDOX
+--  @metapackage
+--  @changelog:
+--   + Yeah, I did that.
+--  @about:
+--  * Testing, I did that.
+--  @provides:
+--      [main] Arrange View\Automation Item\Adjustment\Reaper Studio - Automation Items - Adjustment - Length - End - Extend - Grid.lua
+--      [main] Arrange View\Automation Item\Adjustment\Reaper Studio - Automation Items - Adjustment - Length - End - Shorten - Grid.lua
+--      [main] Arrange View\Automation Item\Adjustment\Reaper Studio - Automation Items - Adjustment - Length - Start - Extend - Grid.lua
+--      [main] Arrange View\Automation Item\Adjustment\Reaper Studio - Automation items - Adjustment - Length - Start - Shorten - Grid.lua
+--      [main] Arrange View\Automation Item\Adjustment\Reaper Studio - Automation Items - Adjustment - Position - Left - Grid.lua
+--      [main] Arrange View\Automation Item\Adjustment\Reaper Studio - Automation Items - Adjustment - Position - Right - Grid.lua
+--  @link: https://github.com/BIXI4DOX/Reaper_Studio
