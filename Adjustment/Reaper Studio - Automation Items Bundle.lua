a14e0bcdf17d923bd1074f756c8af4990ed380a5
BIXI DOX <bixidox4@gmail.com>
2025-11-29 21:52:39 +0700

    Update Reaper Studio - Automation Items Bundle.lua
    I DID THAT!

--- Adjustment/Reaper Studio - Automation Items Bundle.lua
+++ Adjustment/Reaper Studio - Automation Items Bundle.lua
@@ -1,18 +1,19 @@
---  @description: Reaper Studio - Automation Items Bundle
+--  @description Reaper Studio - Automation Items Bundle
 --  @version 1.0
 --  @author BIXIDOX
 --  @metapackage
---  @changelog:
+--  @changelog
 --   + Yeah, I did that.
---  @about:
+--  @about
 --  * Testing, I did that.
---  @provides:
+--  @provides
 --      [main] Adjustment\Reaper Studio - Automation Items - Adjustment - Length - End - Extend - Grid.lua
 --      [main] Adjustment\Reaper Studio - Automation Items - Adjustment - Length - End - Shorten - Grid.lua
 --      [main] Adjustment\Reaper Studio - Automation Items - Adjustment - Length - Start - Extend - Grid.lua
 --      [main] Adjustment\Reaper Studio - Automation items - Adjustment - Length - Start - Shorten - Grid.lua
 --      [main] Adjustment\Reaper Studio - Automation Items - Adjustment - Position - Left - Grid.lua
 --      [main] Adjustment\Reaper Studio - Automation Items - Adjustment - Position - Right - Grid.lua
---  @link: https://github.com/BIXI4DOX/Reaper_Studio
+--  @link https://github.com/BIXI4DOX/Reaper_Studio
+
 
 
