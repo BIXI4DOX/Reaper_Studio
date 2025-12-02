b4a593c944040475b8eed8f4af3937c50136572e
BIXI DOX <bixidox4@gmail.com>
2025-12-02 13:06:35 +0700

    Now Hopefully this one works
    METAPACKAGING, COme on

---
+++ Adjustment/Reaper Studio - Automation Items Bundle.lua
@@ -0,0 +1,16 @@
+-- @description Reaper Studio - Automation Items Bundle
+-- @version 1.0
+-- @author BIXIDOX
+-- @metapackage
+-- @changelog
+--  + Yeah, I did that.
+-- @about
+--  * Testing, I did that.
+-- @provides
+--	[main] Reaper Studio - Automation Items - Adjustment - Length - End - Extend - Grid.lua
+--	[main] Reaper Studio - Automation Items - Adjustment - Length - End - Shorten - Grid.lua
+--	[main] Reaper Studio - Automation Items - Adjustment - Length - Start - Extend - Grid.lua
+--	[main] Reaper Studio - Automation items - Adjustment - Length - Start - Shorten - Grid.lua
+--	[main] Reaper Studio - Automation Items - Adjustment - Position - Left - Grid.lua
+--	[main] Reaper Studio - Automation Items - Adjustment - Position - Right - Grid.lua
+-- @link https://github.com/BIXI4DOX/Reaper_Studio
